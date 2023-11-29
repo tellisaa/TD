912e8bc87acc8b5bc31915d05e766c111147776b
tellisaa <152372300+tellisaa@users.noreply.github.com>
2023-11-29 17:35:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ws = new WebSocket("wss://websocket-p75n.onrender.com/:443");
+let ws = new WebSocket("wss://me-7641.onrender.com");
 
 let controllTD = document.querySelector('.controllTD') ;
 controllTD.addEventListener('input', (event) => {
